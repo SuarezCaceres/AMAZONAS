56ad380c293d60c7bd595c78ffb5847ad24a1b91
DayronBrusRojas <U23271062@utp.edu.pe>
2026-07-08 22:28:51 -0500

    feat(telemetria): exposicion de endpoints de Actuator y configuracion de acceso en SecurityConfig

--- src/backend/src/main/java/com/amazonas/backend/security/config/SecurityConfig.java
+++ src/backend/src/main/java/com/amazonas/backend/security/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/actuator/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         // WebSocket handshake (SockJS también usa HTTP para negociación)
                         .requestMatchers("/ws/**").permitAll()
